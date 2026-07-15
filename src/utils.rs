5d03d22b8e4d612ee7226ee0db0f64958fbf5671
Hazu <someonecalledkr3my@gmail.com>
2026-07-14 22:33:01 -0600

    features features

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,51 @@
 use std::env;
 use std::fs;
 use std::io;
+use std::io::BufRead;
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 fn help() {}
 
-fn build() {}
+fn build() {
+    let user_env = env::var("USER").unwrap_or_default();
+
+    let base_dir = format!("/home/{}/.refract", user_env);
+    let selected_profile = fs::read_to_string(format!("{}/profile", base_dir))
+        .expect("Error trying to get current profile");
+
+    let profile_dir = format!("{}/profiles/{}", base_dir, selected_profile);
+    let linker_path = PathBuf::from(&profile_dir).join("linker");
+    let linker_file = fs::File::open(&linker_path).expect("Error trying to open linker file");
+    let reader = io::BufReader::new(linker_file);
+
+    for line in reader.lines() {
+        let line = line.unwrap_or("".to_string());
+
+        if !line.is_empty() {
+            let mut parts: Vec<String> = line.split(" -> ").map(|x| x.to_string()).collect();
+
+            parts[0] = format!("{}/{}", profile_dir, &parts[0]);
+
+            if parts[1].starts_with("~") {
+                parts[1] = format!("/home/{}/{}", user_env, &parts[1][2..]);
+            }
+
+            let path = Path::new(parts[1].as_str());
+
+            if path.exists() && path.is_dir() && fs::metadata(path).is_ok() {
+                fs::remove_dir_all(path).expect("Error trying to replace directory");
+            }
+
+            Command::new("cp")
+                .arg("-r")
+                .arg(parts[0].as_str().trim())
+                .arg(parts[1].as_str().trim())
+                .status()
+                .expect("Error trying to link file or directory");
+        }
+    }
+}
 
 fn profile(new_profile: &String) {
     let base_dir = format!("/home/{}/.refract", env::var("USER").unwrap_or_default());
